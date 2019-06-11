8b2d3dbe06def53e7870b59334036ed2ab864159
av3ng3roo <av3ng3roo@gmail.com>
2019-06-11 02:04:53 +0200

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-tailwind-styled-components-starter',
-        short_name: 'starter',
+        name: 'ddev',
+        short_name: 'ddev',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.ico',
       },
     },
     'gatsby-plugin-offline',
