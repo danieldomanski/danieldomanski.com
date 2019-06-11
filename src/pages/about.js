6077b7e8fa67697a5553ee6320738799b0f235aa
av3ng3roo <av3ng3roo@gmail.com>
2019-06-12 00:17:22 +0200

    basic atomic folder structure with some features

---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const About = () => <div>abc</div>
+
+export default About
---
+++ src/pages/projects.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Projects = () => <div>Index page</div>
+
+export default Projects
